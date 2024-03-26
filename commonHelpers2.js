037b26e7480f0c96417cf70588c5d05899880e95
Mariia2308 <136304734+Mariia2308@users.noreply.github.com>
2024-03-26 20:33:46 +0000

    Deploying to gh-pages from @ Mariia2308/booki@bf73ad03796ed229940bc379948e692684e816b0 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,24 +1,24 @@
-import{i as r,a as p,T as n,g as d}from"./assets/switch-button-5d4f229b.js";import"./assets/vendor-f87d3a68.js";const m="/JS-new_teamProject/assets/IMG_9606-a8e7c57b.png",h="/JS-new_teamProject/assets/amazon-2651c11a.svg",u="/JS-new_teamProject/assets/book-5e54f05d.svg",k="/JS-new_teamProject/assets/trash-121ef440.svg",e=document.querySelector(".shoppinglist-container"),l=`
+import{i as r,a as p,T as n,g as d}from"./assets/switch-button-5d4f229b.js";import"./assets/vendor-f87d3a68.js";const h="/booki/assets/IMG_9606-a8e7c57b.png",m="/booki/assets/amazon-2651c11a.svg",k="/booki/assets/book-5e54f05d.svg",b="/booki/assets/trash-121ef440.svg",i=document.querySelector(".shoppinglist-container"),l=`
     <div class="shoppinglist-blocks">
         <h2 class="text">This page is empty, add some books and proceed to order.</h2>
-        <img src="${m}" alt="Shopping Image" class="shoppinglist-img96061">
+        <img src="${h}" alt="Shopping Image" class="shoppinglist-img96061">
     </div>
-`;async function S(){try{const t=r(n);if(!t||!t.length){e.innerHTML=l;return}let s="";for(const o of t){const a=await d(o.constID);s+=f(a,o)}e.innerHTML=s}catch(t){console.error("Error fetching book data:",t)}}function f(t,s){return`
-    <div class="container-block" id="${s.constID}">
+`;async function u(){try{const s=r(n);if(!s||!s.length){i.innerHTML=l;return}let t="";for(const o of s){const a=await d(o.constID);t+=f(a,o)}i.innerHTML=t}catch(s){console.error("Error fetching book data:",s)}}function f(s,t){return`
+    <div class="container-block" id="${t.constID}">
         <div class="btn-and-links">
-            <button class="trash-btn"><img src="${k}" alt=""></button>
+            <button class="trash-btn"><img src="${b}" alt=""></button>
             <ul class="links">
-                <li><img src="${h}" class="amazon"></li>
-                <li><img src="${u}"></li>
+                <li><img src="${m}" class="amazon"></li>
+                <li><img src="${k}"></li>
             </ul>
         </div>
-        <img src="${t.book_image}" alt="${t.title}" class="book-image">
+        <img src="${s.book_image}" alt="${s.title}" class="book-image">
         <div class="text-area">
-            <h2 class="shopping-list-title">${t.title}</h2>
-            <h2 class="shopping-list-title-name">${t.list_name}</h2>
-            <p class="shopping-list-description">${t.description}</p>
-            <h2 class="shopping-list-author">${t.author}</h2>
+            <h2 class="shopping-list-title">${s.title}</h2>
+            <h2 class="shopping-list-title-name">${s.list_name}</h2>
+            <p class="shopping-list-description">${s.description}</p>
+            <h2 class="shopping-list-author">${s.author}</h2>
         </div>
     </div>
-  `}e.addEventListener("click",function(t){const s=t.target;if(t.target.parentNode.nodeName==="BUTTON"||t.target.nodeName==="BUTTON"){const o=s.closest(".container-block"),a=o.getAttribute("id");let i=[];const g=r(n);for(const c of g)c.constID!==a&&i.push(c);p(n,i),o.remove(),e.querySelector(".container-block")||(e.innerHTML=l)}});S();
+  `}i.addEventListener("click",function(s){const t=s.target;if(s.target.parentNode.nodeName==="BUTTON"||s.target.nodeName==="BUTTON"){const o=t.closest(".container-block"),a=o.getAttribute("id");let e=[];const g=r(n);for(const c of g)c.constID!==a&&e.push(c);p(n,e),o.remove(),i.querySelector(".container-block")||(i.innerHTML=l)}});u();
 //# sourceMappingURL=commonHelpers2.js.map
